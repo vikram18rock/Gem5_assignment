8ddeafacd5fef81bb55bb630490c807861fb87ba
Aadarsh <v16ash@gmail.com>
2024-10-29 18:06:33 +0530

    Define clk_domain of system
    set to 3GHz as required

--- configs/assg.py
+++ configs/assg.py
@@ -4,3 +4,10 @@ from m5.objects import *
 
 # instantiating system
 system = System()
+
+# Set clock of instantiated system
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '3GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+
