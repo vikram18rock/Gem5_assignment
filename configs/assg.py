26349a7d91160967cdd15cd9025093402e3cda36
Aadarsh <v16ash@gmail.com>
2024-10-29 18:09:27 +0530

    Add main memory
    Adding 4gb main memory to system

--- configs/assg.py
+++ configs/assg.py
@@ -10,4 +10,6 @@ system.clk_domain = SrcClockDomain()
 system.clk_domain.clock = '3GHz'
 system.clk_domain.voltage_domain = VoltageDomain()
 
-
+# creating 4GB main memory
+system.mem_mode = 'timing'
+system.mem_ranges = [AddrRange('4GB')]
